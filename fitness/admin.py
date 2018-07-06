3904dce51eb172cfb9ca7fccadb2c4ec64642649
Herman Saksono <hsaksono@ccs.neu.edu>
2018-07-06 19:21:59 -0400

    expose ActivityByDay to the admin

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ActivityByMinute
+from .models import ActivityByMinute, ActivityByDay
 
 # Register your models here.
-admin.site.register(ActivityByMinute)
+admin.site.register(ActivityByMinute, ActivityByDay)
