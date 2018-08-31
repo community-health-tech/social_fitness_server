fe98e36a32ba99c5c409c40bade2003accf7bea3
Herman Saksono <hsaksono@ccs.neu.edu>
2018-08-30 23:02:53 -0400

    no message

--- people/api.py
+++ people/api.py
@@ -127,8 +127,24 @@ class PersonInfo(APIView):
     GET request returns the Person's info.
     """
     permission_classes = (permissions.IsAuthenticated,)
+    parser_classes = (JSONParser,)
 
     def get(self, request, person_id, format=None):
+        logged_person = get_person(request.user.id)
+        group = get_group(logged_person)
+
+        if person_id == "-":
+            serializer = PersonSerializer(logged_person)
+            return Response(serializer.data)
+        elif Membership.is_member(group, person_id) is False:
+            output = {"message": "Not authorized"}
+            return Response(output, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            person = Membership.get_member(group, person_id)
+            person_meta = person.get_meta()
+            return Response(json.loads(person_meta.profile_json))
+
+    def get2(self, request, person_id, format=None):
         logged_person = get_person(request.user.id)
         group = get_group(logged_person)
 
