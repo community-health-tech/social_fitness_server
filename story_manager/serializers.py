1d13e74cc2ea90137f92c63231a5ecffef87afa7
Herman Saksono <herman.saksono@gmail.com>
2019-01-30 12:24:35 -0500

    update model to expose story is_locked and story next_id

--- story_manager/serializers.py
+++ story_manager/serializers.py
@@ -13,11 +13,13 @@ class GroupStorySerializer(serializers.ModelSerializer):
     title= serializers.ReadOnlyField(source="story.title")
     cover_url = serializers.ReadOnlyField(source="story.cover_url")
     def_url = serializers.ReadOnlyField(source="story.def_url")
+    is_locked = serializers.ReadOnlyField(source="story.is_locked")
+    next_story_id = serializers.ReadOnlyField(source="story.next_story_id")
 
     class Meta:
         model = GroupStory
         fields = ('id', 'title', 'cover_url', 'def_url', 'is_current',
-                  'current_page')
+                  'current_page', 'is_locked', 'next_story_id')
 
     def update(self, instance, validated_data):
         instance.is_current = validated_data.get('is_current', instance.is_current)
