a8843d85609b4fddf28685b7fdbfcb08536c29b0
Herman Saksono <herman.saksono@gmail.com>
2019-02-05 11:51:23 -0500

    fix a typo in the fitbit serializers

--- fitness_connector/serializers.py
+++ fitness_connector/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class PersonFitnessSyncResultSerializer(serializers.Serializer):
     person_id = serializers.IntegerField()
-    last_pull_time = serializers.DateTimeField()
+    last_sync_time = serializers.DateTimeField()
