8d6ae6b0592d9a2f2de73c52add01724ef1caf83
Herman Saksono <herman.saksono@gmail.com>
2019-03-20 18:21:50 -0400

    Update the steps defaults

--- challenges/constants.py
+++ challenges/constants.py
@@ -1,9 +1,9 @@
-DEFAULT_STEPS = 400  # type: int
+DEFAULT_STEPS = 1000  # type: int
 DEFAULT_CALORIES = 1800  # type: float
 DEFAULT_DISTANCE = 2  # type: float
 DEFAULT_ACTIVE_MINUTES = 5  # type: int
 
-MIN_STEPS = 300  # type: int
+MIN_STEPS = 1000  # type: int
 MIN_CALORIES = 1600  # type: float
-MIN_DISTANCE = 0.5  # type: float
-MIN_ACTIVE_MINUTES = 0  # type: int
+MIN_DISTANCE = 2  # type: float
+MIN_ACTIVE_MINUTES = 5  # type: int
